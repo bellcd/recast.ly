fe7b91ec890b8f2721388372b2a2f96a2c715fc0
Manuela Gomez <mmanu.gomez@gmail.com>
2019-09-06 00:19:53 -0400

    [wip] Hooked Video List and Video Player, all tests passing for those

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,16 @@ class App extends React.Component {
       videos: exampleVideoData,
       currentVideo: exampleVideoData[0]
     };
+    // styling best practice: put bind functions below state
+    this.onVideoClick = this.onVideoClick.bind(this);
   }
+
+  onVideoClick (video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+
   render() {
     return (
       <div>
@@ -29,17 +38,27 @@ class App extends React.Component {
           <div className="col-md-7">
             <div>
               <h5>
-                <em>{this.state.currentVideo.snippet.title}</em>
+                {/* I commented this out because I think it was also a place holder */}
+                {/* <em>{this.state.currentVideo.snippet.title}</em> */}
                 <VideoPlayer video={this.state.currentVideo} />
               </h5>
             </div>
           </div>
           <div className="col-md-5">
             {/* because of the way props is defined on VideoList, it has to be called "videos", see line 14 of that file */}
+            {/* when you bind the click to the div with the video list, you can click on the titles */}
             <div>
+              {/* here is another way to not have to bind an onClick event on a component with line 16 above */}
+              {/* <button onClick={() => this.onVideoListEntryClick}></button> */}
               <h5>
-                <em>videoList</em>
-                <VideoList videos={this.state.videos} />
+                {/* I commented this out because I think this was more of a place holder <em>videoList</em> */}
+                {/* This is the tricky part, first you have to realize that in order for the
+                    actual VideoListEntry component to get access to the onVideoClick fucntion
+                    you'll have to pass down that function from the parent - App - to its child
+                    - the Video List component - and then, to Video List Entry. But in order to do that
+                    You have to create a new prop to hold this info and you'll have to create it
+                    on both children */}
+                <VideoList videos={this.state.videos} videoClick={this.onVideoClick} />
               </h5>
             </div>
           </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,13 @@
 import VideoListEntry from "./VideoListEntry.js";
 
+// props passed in were videos={this.state.videos} and videosClick={[function]}
 var VideoList = (props) => (
-  <div className="video-list">
+  <div className="video-list" >
     {props.videos.map((video) => {
       // similar to what happend in App, we have to pass the right props to VideListEntry
-      return <VideoListEntry video={video}/>;
+      // key is like a "private" prop type - that's how we can do a fix for the
+      // ERROR: react.js:18798 Warning: Each child in an array or iterator should have a unique "key" prop.
+      return <VideoListEntry video={video} key={video.id.videoId} videoClick={props.videoClick}/>;
       // had to take off the code before <VideoListEntry... and add it to VideoListEntry file on line 8
       // in order for it to pass the tests <div><h5><em>{video.snippet.title}</em></h5> ...</div>
     })}
@@ -14,7 +17,11 @@ var VideoList = (props) => (
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videos: React.PropTypes.array.isRequired,
+  // we want to not name our prop that will take in our clicking event handling function
+  // the same as "onClick" because that is a naming convention reserved for that event
+  // happening on the actual (html) component - See VideListEntry line 5
+  videoClick: React.PropTypes.func.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
